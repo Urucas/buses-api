f2bafe55968f6ba2eaf3aa3ab70e371cc59c2980
vrunoa <vruno.alassia@gmail.com>
2016-02-29 10:59:23 -0300

    add get cities method

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,31 @@
-import index from "../lib/"
-describe("Test foo is bar and bar is foo", () => {
-  
-  it("foo should be bar", (done) => {
-    let bar = index("foo") 
-    if(bar != "bar")
-      throw new Error("expected value: bar actual: "+bar);
-    done();
-  })
+import buses from "../lib/"
+import chai from 'chai'
+let should = chai.should()
+let expect = chai.expect
 
-  it("bar should be foo", (done) => {
-    let foo = index("bar") 
-    if(foo != "foo")
-      throw new Error("expected value: foo actual: "+foo);
-    done();
+describe("API tests", () => {
+  
+  it("api should be an object", (done) => {
+    buses.should.not.equal(null)
+    buses.should.not.equal(undefined)
+    expect(buses).to.be.a("object")
+    done()
+  })
+  
+  it("cities should return a promise", (done) => {
+    buses.cities.should.not.equal(null)
+    buses.cities.should.not.equal(undefined)
+    expect(buses.cities).to.be.a("function")
+    expect(buses.cities()).to.be.a("promise")
+    done()    
+  })
+  
+  it("expect cities to return an array", (done) => {
+    buses.cities().then( (cities) => {
+      cities.should.not.equal(null)
+      cities.should.not.equal(undefined)
+      expect(cities).to.be.a("array")
+      done()  
+    })
   })
 })
